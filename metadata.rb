20c1f5d2160b85600ff04554873af60e17107b2a
Zac Grierson <zac@pentascape.com>
2016-12-20 09:33:32 +0000

    Inital Steps

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name             'laravel-application-opsworks'
+maintainer       'DarkRhino Limited'
+maintainer_email 'dev@darkrhino.com'
+license          'All rights reserved'
+description      'Installs/Configures ninja'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+
+depends          'nginx'
+depends          'php-fpm'
+depends          'mariadb'
